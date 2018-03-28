5519ed7d41acf3937a36a84e8ccefa481117dbb7
SixSins <sixsixsixsins@gmail.com>
2018-03-27 21:52:34 -0500

    Webpack stuff Build Sass with Webpack Use dev server with HMR Webroot should be in dist folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { render } from "react-dom";
 import Locator from "./Locator/index";
+import "./style.scss";
 
 render(<Locator />, document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+let path = require("path");
+let ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+let extractSass = new ExtractTextPlugin({
+  filename: "style.css",
+  disable: process.env.NODE_ENV.toLowerCase() === "development"
+});
+
+let config = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname) + "/dist",
+    filename: "bundle.js"
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: "ts-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                outputStyle: "compressed",
+                sourceMap: true
+              }
+            }
+          ],
+          fallback: "style-loader"
+        }),
+      }
+    ]
+  },
+  plugins: [
+    extractSass
+  ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".scss"]
+  },
+  devServer: {
+    contentBase: "dist"
+  }
+}
+
+module.exports = config;
